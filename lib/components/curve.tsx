7b464467e84da7e90744404d54e091e887813db2
francisco risso <frisso2001@gmail.com>
2025-07-06 21:48:30 -0300

    enhancement: Add bezier curves to links

--- lib/components/curve.tsx
+++ lib/components/curve.tsx
@@ -1,14 +1,31 @@
 const Curve = ({ x1,y1,x2,y2 }: { x1: number; y1: number; x2: number; y2: number }) => {
+  const deltaX = Math.abs(x2 - x1) * 0.5;
+  const deltaY = Math.abs(y2 - y1) * 0.5;
 
-  const controlX = (x1 + x2) / 2;
-  const controlY = Math.min(y1, y2) - 50; 
+  let pathData = '';
 
-  const pathData = `M ${x1} ${y1}, ${x2} ${y2}`;
+  if (Math.abs(x2 - x1) < 40) {
+    const controlY1 = y1 + deltaY;
+    const controlY2 = y2 - deltaY;
+
+    pathData = `M ${x1},${y1}
+      C ${x1},${controlY1}
+        ${x2},${controlY2}
+        ${x2},${y2}`;
+  } else {
+    pathData = `M ${x1},${y1}
+      C ${x1 + deltaX},${y1}
+        ${x2 - deltaX},${y2}
+        ${x2},${y2}`;
+  }
 
   return (
-   
-      <path d={pathData} stroke="#fefefe" fill="none" strokeWidth="1" />
-
+    <path
+      d={pathData}
+      stroke="#fefefe"
+      fill="none"
+      strokeWidth="1"
+    />
   );
 };
 
