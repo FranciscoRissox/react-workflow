fb721a75d8a9458fb20d936251a81f653735e94b
francisco risso <frisso2001@gmail.com>
2025-07-10 22:55:41 -0300

    Fix base path

--- vite.docs.config.ts
+++ vite.docs.config.ts
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/react-workflow/',
   build: {
-    outDir: 'docs'
+    outDir: 'docs',
   },
 });
