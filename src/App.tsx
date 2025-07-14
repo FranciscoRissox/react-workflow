4f350e94eca905924bab62befeb12b9057bf53a5
francisco risso <frisso2001@gmail.com>
2025-07-13 23:08:27 -0300

    Bootstrap demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,61 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import './style.css'
+import { useWorkflow, WorkFlow } from "@franciscorissox/react-workflow";
+
+const baseNode: any = {
+  position:{
+    x:500,y:500
+  },
+  style:{
+    height:50,
+    width:200,
+    border:'1px solid black',
+    borderRadius:'6px'
+  },
+  enabledSockets:{
+    LEFT:{lineStyle:{strokeWidth:1,strokeColor:'gray'}},
+    RIGHT:{lineStyle:{strokeWidth:1,strokeColor:'gray'}},
+  }
+}
+
+const startNode = {
+  ...baseNode, children: 'Start'
+}
+
+const endNode = {
+  ...baseNode, children: 'End'
+}
+
+const apiNode = {
+  ...baseNode, children: 'Api Call'
+}
+
+const functionNode = {
+  ...baseNode, children: 'Function'
+}
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  const workflow = useWorkflow();
+  
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="w-full h-screen bg-gray-200 flex">
+      <div className='h-screen bg-gray-500 w-1/4 flex flex-col'>
+        <AddNode onAddNode={() => workflow.actions.addNode(startNode)} text="Add Start"/>
+        <AddNode onAddNode={() => workflow.actions.addNode(endNode)} text="Add End"/>
+        <AddNode onAddNode={() => workflow.actions.addNode(apiNode)} text="Add Api call"/>
+        <AddNode onAddNode={() => workflow.actions.addNode(functionNode)} text="Add Function"/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className='w-3/4'>
+        <WorkFlow {...workflow} width='800px' height='800px'/>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
+  )
+}
+
+const AddNode = ({onAddNode,text}: {onAddNode: (node: any) => void, text: string}) => {
+  return (
+    <button className='bg-blue-500 text-white p-2 rounded mt-2 mx-2' onClick={() => onAddNode({type: 'node', data: {label: text}})}>{text}</button>
   )
 }
 
